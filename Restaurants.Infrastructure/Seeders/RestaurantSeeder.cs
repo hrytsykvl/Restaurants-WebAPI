c1e21c8046bf3d07e02434575f12717fb5558b41
hrytsykvl <gricikvlad@gmail.com>
2024-08-29 17:57:48 +0300

    Implement Database connection and seed data to restaurants table

---
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -0,0 +1,76 @@
+﻿using Restaurants.Domain.Entities;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Seeders
+{
+    internal class RestaurantSeeder(RestaurantsDbContext context) : IRestaurantSeeder
+    {
+        public async Task Seed()
+        {
+            if (await context.Database.CanConnectAsync())
+            {
+                if (!context.Restaurants.Any())
+                {
+                    var restaurants = GetRestaurants();
+                    context.Restaurants.AddRange(restaurants);
+                    await context.SaveChangesAsync();
+                }
+            }
+        }
+
+        private IEnumerable<Restaurant> GetRestaurants()
+        {
+            List<Restaurant> restaurants = [
+            new()
+            {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description =
+                    "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered in Louisville, Kentucky, that specializes in fried chicken.",
+                ContactEmail = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes =
+                [
+                    new ()
+                    {
+                        Name = "Nashville Hot Chicken",
+                        Description = "Nashville Hot Chicken (10 pcs.)",
+                        Price = 10.30M,
+                    },
+
+                    new ()
+                    {
+                        Name = "Chicken Nuggets",
+                        Description = "Chicken Nuggets (5 pcs.)",
+                        Price = 5.30M,
+                    },
+                ],
+                Address = new ()
+                {
+                    City = "New York",
+                    Street = "Broad St 5",
+                    ZipCode = "WC2N 5DU"
+                },
+
+            },
+            new ()
+            {
+                Name = "McDonalds",
+                Category = "Fast Food",
+                Description =
+                    "McDonald's Corporation (McDonald's), incorporated on December 21, 1964, operates and franchises McDonald's restaurants.",
+                ContactEmail = "contact@mcdonald.com",
+                HasDelivery = true,
+                Address = new Address()
+                {
+                    City = "New York",
+                    Street = "Madison 193",
+                    ZipCode = "W1F 8SR"
+                }
+            }
+        ];
+
+            return restaurants;
+        }
+    }
+}
