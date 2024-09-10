3b8e8a61b198a32c614e4bd68f79a1901a42a579
hrytsykvl <gricikvlad@gmail.com>
2024-09-10 20:30:54 +0300

    Added owner property for restaurant

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Restaurants.Application.Restaurants.DTO;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.RepositoryContracts;
 
@@ -9,13 +10,17 @@ namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant
 {
     public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger,
         IMapper mapper,
-        IRestaurantsRepository restaurantsRepository) : IRequestHandler<CreateRestaurantCommand, int>
+        IRestaurantsRepository restaurantsRepository,
+        IUserContext userContext) : IRequestHandler<CreateRestaurantCommand, int>
     {
         public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
         {
-            logger.LogInformation("Creating restaurant");
+            var currentUser = userContext.GetCurrentUser();
+
+            logger.LogInformation($"{currentUser.Email} [{currentUser.Id}] is creating a new restaurant {request}");
 
             var restaurant = mapper.Map<Restaurant>(request);
+            restaurant.OwnerId = currentUser.Id;
 
             int id = await restaurantsRepository.CreateRestaurant(restaurant);
 
