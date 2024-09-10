9f11416f70351d4b4231663978e3f8889d7c8967
hrytsykvl <gricikvlad@gmail.com>
2024-09-10 18:45:26 +0300

    Implemented role-based authorization, assign and unassign user role endpoints

--- Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -1,4 +1,6 @@
-﻿using Restaurants.Domain.Entities;
+﻿using Microsoft.AspNetCore.Identity;
+using Restaurants.Domain.Constants;
+using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Persistence;
 
 namespace Restaurants.Infrastructure.Seeders
@@ -15,7 +17,35 @@ namespace Restaurants.Infrastructure.Seeders
                     context.Restaurants.AddRange(restaurants);
                     await context.SaveChangesAsync();
                 }
+
+                if(!context.Roles.Any())
+                {
+                    var roles = GetRoles();
+                    context.Roles.AddRange(roles);
+                    await context.SaveChangesAsync();
+                }
             }
+        }
+
+        private IEnumerable<IdentityRole> GetRoles()
+        {
+            List<IdentityRole> roles =
+                [
+                    new (UserRoles.User)
+                    {
+                        NormalizedName = UserRoles.User.ToUpper()
+                    },
+                    new (UserRoles.Owner)
+                    {
+                        NormalizedName = UserRoles.Owner.ToUpper()
+                    },
+                    new (UserRoles.Admin)
+                    {
+                        NormalizedName = UserRoles.Admin.ToUpper()
+                    },
+                ];
+
+            return roles;
         }
 
         private IEnumerable<Restaurant> GetRestaurants()
